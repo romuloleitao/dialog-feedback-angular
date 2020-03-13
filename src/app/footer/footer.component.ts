65c0085d5ce4fa1f477039c34cea4f7ac95e4fdb
Romulo leitao <romulojleitao@gmail.com>
2020-03-13 17:25:51 -0400

    Footer message changed with author name.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  author = "Romulo Leitao";
+  footerMessage = "© Copyright 2020. Made by " + this.author + ". All rights reserved.";
+
   constructor() { }
 
   ngOnInit(): void {
